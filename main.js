c3793b4e892ad5066888cc35729998f5be0a7b18
suhanabolt <106386090+suhanabolt@users.noreply.github.com>
2022-06-28 18:47:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function preload() 
+{
+
+}
+function setup() 
+{
+    canvas=createCanvas(400,400)
+    canvas.position(500,200);
+}
+function draw() 
+{
+    
+}
+function take_snapshot() 
+{
+    save('myJokerImage.png')
+}
